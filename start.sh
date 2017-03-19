b1b8d489cc1fed8fced9933e7431fe5016fc169d
Oleg Kulik <okulik@gorillagroup.com>
2017-03-19 17:40:16 +0200

    adjust ssl certs gen

--- start.sh
+++ start.sh
@@ -4,27 +4,22 @@ mkdir -p /etc/nginx/certs
 
 chown -R www-data:www-data /etc/nginx/certs
 
-# traverse hosts
-# if hosts are set
-if [ ! -z $VIRTUAL_HOST ]
+# if cert name is set
+if [ -n "$CERT_NAME" ]
 then
-    IFS=',';
-    for host in $VIRTUAL_HOST
-    do
-        if [ ! -f "/etc/nginx/certs/${host}.key" ]
-        then
-            # if key file does not exist create it
-            openssl req \
-                -x509 \
-                -sha256 \
-                -nodes \
-                -days 365 \
-                -newkey rsa:2048 \
-                -subj "/C=US/ST=Illinois/L=Chicago/O=Dis/CN=${host}" \
-                -keyout "/etc/nginx/certs/${host}.key" \
-                -out "/etc/nginx/certs/${host}.crt"
-        fi
-    done
+    if [ ! -f "/etc/nginx/certs/${CERT_NAME}.key" ]
+    then
+        # if key file does not exist create it
+        openssl req \
+            -x509 \
+            -sha256 \
+            -nodes \
+            -days 365 \
+            -newkey rsa:2048 \
+            -subj "/C=US/ST=Illinois/L=Chicago/O=Dis/CN=${CERT_NAME}" \
+            -keyout "/etc/nginx/certs/${CERT_NAME}.key" \
+            -out "/etc/nginx/certs/${CERT_NAME}.crt"
+    fi
 fi
 
 exec "$@"
